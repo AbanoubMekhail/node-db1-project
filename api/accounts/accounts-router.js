4bcab087394a944d8d8d9e0dc27f6740f88b1245
abanoub nagib <abanoub.nagib@hotmail.com>
2023-10-31 20:19:00 -0700

    del endpoint

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -22,11 +22,11 @@ router.post(
   '/',
   md.checkAccountPayload,
   md.checkAccountNameUnique,
-  (req, res, next) => {
+  async (req, res, next) => {
     // DO YOUR MAGIC
     try {
-
-      res.json('post an account')
+      const newAcc = await Account.create(req.body)
+      res.status(201).json(newAcc)
     } catch (err) {
       next(err)
     }
@@ -47,11 +47,11 @@ router.put(
     }
   });
 
-router.delete('/:id', md.checkAccountId, (req, res, next) => {
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-
-    res.json('delete accounts')
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   } catch (err) {
     next(err)
   }
